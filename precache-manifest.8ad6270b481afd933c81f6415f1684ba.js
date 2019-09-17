9702b1ee9bb707f3831ad17413b71e749d726b23
bfsiumm <147061@supinfo.com>
2019-09-17 22:32:29 +0200

    Updates

---
+++ precache-manifest.8ad6270b481afd933c81f6415f1684ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "150ce9f27b93cb5b1d6b315dc52ce86f",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "26d29d9359c4ed380885",
+    "url": "/monster-rolodex/static/css/main.144f54fc.chunk.css"
+  },
+  {
+    "revision": "cf82e60825fd6ea89bdf",
+    "url": "/monster-rolodex/static/js/2.9b9d306e.chunk.js"
+  },
+  {
+    "revision": "26d29d9359c4ed380885",
+    "url": "/monster-rolodex/static/js/main.e0d0c455.chunk.js"
+  },
+  {
+    "revision": "ac59eee0e1a1414be595",
+    "url": "/monster-rolodex/static/js/runtime~main.fe0f33f5.js"
+  }
+]);
